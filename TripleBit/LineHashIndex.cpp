c854f8e02fa88b473d0732d73d57d70dc424fe23
qingqingxu <qingqingxuv@163.com>
2017-06-05 21:12:25 +0800

    modified:   TripleBit/LineHashIndex.cpp

--- TripleBit/LineHashIndex.cpp
+++ TripleBit/LineHashIndex.cpp
@@ -267,12 +267,12 @@ Status LineHashIndex::buildIndex(unsigned chunkType)
 #ifdef MYDEBUG
 	ofstream out;
 	out.open("buildindex", ios::app);
-	cout << "minID: " << minID << "\tmaxID: " << maxID <<endl;
+	out << "minID: " << minID << "\tmaxID: " << maxID <<endl;
 	const uchar* tmp = reader;
 	tmp += sizeof(MetaData);
 	int xynums = (metaData->usedSpace - sizeof(MetaData))/8;
 	for(int i = 0; i < xynums; i++){
-		cout << "x: " << *(ID*)tmp << "\ty: " << *(ID*)(tmp+4) << endl;
+		out << "x: " << *(ID*)tmp << "\ty: " << *(ID*)(tmp+4) << endl;
 		tmp += 8;
 	}
 	out.close();
