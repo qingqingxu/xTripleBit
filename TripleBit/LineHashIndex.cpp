9aa1085cce33eba6d18035726d0a07a7cdfe4e96
qingqingxu <qingqingxuv@163.com>
2017-06-07 15:26:15 +0800

    modified:   TripleBit/LineHashIndex.cpp

--- TripleBit/LineHashIndex.cpp
+++ TripleBit/LineHashIndex.cpp
@@ -366,7 +366,7 @@ size_t LineHashIndex::searchChunkFrank(ID id)
 			cout << mid << endl;
 		}
 		if (MetaID(mid) < id){
-			low = mid;
+			low = mid + 2;
 		}
 		else if (MetaID(mid) > id){
 			high = mid;
