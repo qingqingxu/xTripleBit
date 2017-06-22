960d787a7e98dfee6ad90e8b99871525c8e64ebc
qingqingxu <qingqingxuv@163.com>
2017-06-22 11:18:22 +0800

    modified:   TripleBit/BitmapBuffer.cpp

--- TripleBit/BitmapBuffer.cpp
+++ TripleBit/BitmapBuffer.cpp
@@ -801,7 +801,7 @@ Status ChunkManager::resize(size_t &pageNo) {
 #ifdef MYDEBUG
 	ofstream out;
 	out.open("ChunkManagerresize", ios::app);
-	out << meta->pid << "----------" << usedPages.size() << endl;
+	out << meta->soType << "--------" << meta->pid << "----------" << usedPages.size() << endl;
 	out.close();
 #endif
 
