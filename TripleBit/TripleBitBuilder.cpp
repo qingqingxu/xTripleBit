777d856963b5ff2b1aaf103308aa71685bf34dfb
qingqingxu <qingqingxuv@163.com>
2017-06-23 16:13:47 +0800

    modified:   TripleBit/TripleBitBuilder.cpp

--- TripleBit/TripleBitBuilder.cpp
+++ TripleBit/TripleBitBuilder.cpp
@@ -325,11 +325,9 @@ Status TripleBitBuilder::resolveTriples(TempFile& rawFacts, TempFile& facts) {
 	size_t count1 = 0;
 	TempFile sortedBySubject("./SortByS"), sortedByObject("./SortByO");
 	Sorter::sort(rawFacts, sortedBySubject, skipIdIdId, compare123);
-/*
 #ifdef MYDEBUG
 	print(sortedBySubject, "sortedBySubject_temp");
 #endif
-*/
 	{
 		//insert into chunk
 		sortedBySubject.close();
@@ -384,11 +382,9 @@ Status TripleBitBuilder::resolveTriples(TempFile& rawFacts, TempFile& facts) {
 	//sort
 	cerr << "Sort by Object" << endl;
 	Sorter::sort(rawFacts, sortedByObject, skipIdIdId, compare321);
-/*
 #ifdef MYDEBUG
 	print(sortedByObject, "sortedByObject_temp");
 #endif
-*/
 	{
 		//insert into chunk
 		sortedByObject.close();
