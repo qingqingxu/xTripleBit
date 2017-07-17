b8b2a60dc0c794a639d779f128350206e9c82405
qingqingxu <qingqingxuv@163.com>
2017-07-17 16:07:28 +0800

    d

--- TripleBit/EntityIDBuffer.cpp
+++ TripleBit/EntityIDBuffer.cpp
@@ -239,7 +239,7 @@ Status MidResultBuffer::appendBuffer(const MidResultBuffer *otherBuffer) {
 		cout << __FUNCTION__ << endl;
 #endif
 	if (otherBuffer != NULL) {
-		cout << otherBuffer->resultType << endl;
+		cout << otherBuffer->resultType << "\t" << otherBuffer->objects << endl;
 		switch (otherBuffer->resultType) {
 		case SIGNALID:
 			if (usedSize + otherBuffer->usedSize > totalSize) {
