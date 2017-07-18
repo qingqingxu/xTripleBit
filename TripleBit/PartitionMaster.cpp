8b5747a9f9963ec2017f07fa464530ba5c962d1e
qingqingxu <qingqingxuv@163.com>
2017-07-18 10:04:31 +0800

    s

--- TripleBit/PartitionMaster.cpp
+++ TripleBit/PartitionMaster.cpp
@@ -419,25 +419,25 @@ void PartitionMaster::executeInsertData(SubTrans* subTransaction) {
 	size_t chunkID;
 	shared_ptr<subTaskPackage> taskPackage(new subTaskPackage);
 
-	ofstream s("searchChunkIDByS", ios::app);
+	/*ofstream s("searchChunkIDByS", ios::app);
 	s << "partitionID, " << partitionID << ",subjectID, " << subjectID
-			<< ",object, " << object;
+			<< ",object, " << object;*/
 	chunkID = partitionChunkManager[ORDERBYS]->getChunkIndex()->searchChunk(
 			subjectID, object);
-	s << ",chunkID, " << chunkID << endl;
-	s.close();
+	/*s << ",chunkID, " << chunkID << endl;
+	s.close();*/
 	ChunkTask *chunkTask1 = new ChunkTask(subTransaction->operationType,
 			subjectID, object, objType, subTransaction->triple.scanOperation,
 			taskPackage, subTransaction->indexForTT);
 	taskEnQueue(chunkTask1, xChunkQueue[ORDERBYS][chunkID]);
 
-	ofstream o("searchChunkIDByO", ios::app);
+	/*ofstream o("searchChunkIDByO", ios::app);
 	o << "partitionID, " << partitionID << ",object, " << object
-			<< ",subjectID, " << subjectID;
+			<< ",subjectID, " << subjectID;*/
 	chunkID = partitionChunkManager[ORDERBYO]->getChunkIndex()->searchChunk(
 			object, subjectID);
-	o << ",chunkID, " << chunkID << endl;
-	o.close();
+	/*o << ",chunkID, " << chunkID << endl;
+	o.close();*/
 
 	ChunkTask *chunkTask2 = new ChunkTask(subTransaction->operationType,
 			subjectID, object, objType, subTransaction->triple.scanOperation,
@@ -1103,6 +1103,7 @@ void PartitionMaster::combineTempBufferToSource(TempBuffer *buffer,
 void PartitionMaster::executeChunkTaskDeleteData(ChunkTask *chunkTask,
 		const ID chunkID, const uchar* startPtr, const bool soType) {
 
+/*
 #ifdef MYDEBUG
 	cout << __FUNCTION__ << endl;
 	MetaData* metaData0 =
@@ -1111,6 +1112,7 @@ void PartitionMaster::executeChunkTaskDeleteData(ChunkTask *chunkTask,
 			<< metaData0->max << ",metaData.pageNo," << metaData0->pageNo
 			<< endl;
 #endif
+*/
 
 	ID subjectID = chunkTask->Triple.subjectID;
 	ID tempSubjectID;
